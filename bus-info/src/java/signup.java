1b26a79c4927ce1a273653833758ec4d4c5ad7fb
GuptaHeer27 <guptaheer27@gmail.com>
2025-06-19 17:45:51 +0530

    login.servlet and signup.servlet is edited with full code

--- bus-info/src/java/signup.java
+++ bus-info/src/java/signup.java
@@ -3,13 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
 
 /**
  *
@@ -17,70 +21,43 @@ import jakarta.servlet.http.HttpServletResponse;
  */
 @WebServlet(urlPatterns = {"/signup"})
 public class signup extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+ @Override
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet signup</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet signup at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+         Connection con=null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/delta","root","maheer2709");
+            
+            String name=request.getParameter("name");
+            String email=request.getParameter("email");
+            String username=request.getParameter("username");
+            String password=request.getParameter("password");
+            String gender=request.getParameter("gender");
+            
+            String sql="INSERT INTO users VALUES(?,?,?,?,?)";
+            PreparedStatement pst=con.prepareStatement(sql);
+            
+            pst.setString(1,name);
+                pst.setString(2,email);
+                
+            pst.setString(3,username);
+                
+            pst.setString(4,password);
+                
+            pst.setString(5,gender);
+            
+              
+          pst.executeUpdate();
+        }
+          catch(Exception e){
+              RequestDispatcher rd=request.getRequestDispatcher("error.jsp");
+            rd.forward(request,response);
+            
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+  
 
 }
